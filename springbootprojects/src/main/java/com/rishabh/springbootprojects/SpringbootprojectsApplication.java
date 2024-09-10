295765e8d5f482a5ce9360f735ddaa4394656144
Rishabh <rishabh@cooey.co.in>
2024-09-10 21:54:47 +0530

    Rate limit init

---
+++ springbootprojects/src/main/java/com/rishabh/springbootprojects/SpringbootprojectsApplication.java
@@ -0,0 +1,13 @@
+package com.rishabh.springbootprojects;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootprojectsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootprojectsApplication.class, args);
+	}
+
+}
---
+++ springbootprojects/src/test/java/com/rishabh/springbootprojects/SpringbootprojectsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rishabh.springbootprojects;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootprojectsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
